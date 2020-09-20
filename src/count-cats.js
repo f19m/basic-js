ee48bf0b58c01c7f82290da7e2f2f237e1eac740
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-20 22:12:03 +0500

    feat: task count-cats ready

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(arr) {
+
+  return arr.reduce((prev,item)=>
+      prev + item.map(vl => (vl == '^^') ? 1 : 0).reduce((p, v) => p + v, 0), 0);
+    
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
