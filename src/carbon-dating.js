21d1c366c1b7c2bf65369553c0b582444bfec41e
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-20 23:29:33 +0500

    feat: carbon task finished

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,23 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const log2 = 0.693;
+  
+  
+  if (!(sampleActivity && typeof(sampleActivity) == 'string')) {
+    return false;
+  }
+
+  try{
+    activity = parseFloat(sampleActivity);
+
+    if (activity > 0 && activity < MODERN_ACTIVITY){
+      return Math.ceil(Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD / log2);
+    }
+  }catch(e){
+    return false;
+  }
+
+  return false;
 };
