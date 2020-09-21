d8916979757bf823e38bf6e824bd678ef07d9c9a
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-22 01:01:03 +0500

    feat: transfrom task ready with 1 err

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,74 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform( arr ) {
+  //console.log(arr);
+
+  if (!(Array.isArray(arr))){
+    throw "Input value is not array";
+  }
+  
+
+  let resArr = [];
+  let prevItem=null;
+  let action=null;
+
+  for(let i = 0; i < arr.length; i++){
+  
+      let item = arr[i];
+
+     
+      if (item == '--discard-next' || item == '--discard-prev' ||
+          item == '--double-next' || item == '--double-prev' )
+      {
+        action = item;
+      } else{
+        
+          switch (action) {
+            case '--discard-next':
+              prevItem = null;
+              action = null;
+              break;
+            case '--discard-prev':
+              //console.log('prevItem='+prevItem);
+                if (prevItem){
+               //   console.log('true=');
+                  resArr.splice(resArr.length-1,1);
+                  prevItem = item;
+                }
+                action = null;
+                resArr.push(item);
+                break;
+            case '--double-next':
+                resArr.push(item);
+                resArr.push(item);
+                prevItem = item;
+                action = null;
+                break;
+            case '--double-prev':
+                if (prevItem){
+                  resArr.push(prevItem);
+                }
+                resArr.push(item);
+                prevItem = item;
+                action = null;
+                break;
+            default:
+              resArr.push(item);
+              prevItem = item;
+              break;
+          } 
+
+      
+      
+    }
+
+  }
+
+    
+
+
+    return resArr; 
+
+  
+  
 };
