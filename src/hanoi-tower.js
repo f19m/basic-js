ca64c45a57922bef9fba8d6bb05272a9e498762d
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-21 12:35:32 +0500

    feat: function calculateHanoi ready

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
+  const res = {};
+  
+  //console.log('disksNumber=' + disksNumber + '; turnsSpeed=' + turnsSpeed );
+
+  res.turns = 2 ** disksNumber - 1;
+  res.seconds = Math.floor((res.turns * 3600) / turnsSpeed);
+
+  return res;
+
+   
 };
