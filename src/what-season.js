27804da4292408dcff3380523fc14d37c4e17957
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-21 11:52:14 +0500

    feat: getSeason ready

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  
+  if (!date){
+    return 'Unable to determine the time of year!';
+  }
+
+  try{
+    let newDt = new Date(date);
+    if (typeof(date.getMonth) !== 'function' || date.getTime() != newDt.getTime() ){
+      throw "Error"; 
+    }
+  }catch(e){
+    throw "Error"; 
+  }
+
+  const month = date.getMonth() + 1;
+  if (month >= 1 && month < 3 || month == 12){
+    return 'winter';
+  } else if (month >= 3 && month < 6){
+    return 'spring';
+  }else if (month >= 6 && month < 9){
+    return 'summer';
+  }else if (month >= 9 && month < 12){
+    return 'autumn';
+  }
+
 };
