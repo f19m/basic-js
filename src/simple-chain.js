344eb9a20e896f6f0b7688ac064bee25f566c49e
Омелик Леонид Григорьевич <l.omelik@cft.ru>
2020-09-25 16:26:38 +0500

    feat: chain task ready

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
+  
   getLength() {
-    throw new CustomError('Not implemented');
+    return this.chain.length;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    this.chain.push(value);
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+
+    if (typeof position != 'number'){
+      this.chain = [];
+      throw new CustomError('Input value not a number');
+    } 
+
+    if (!this.chain.hasOwnProperty(position - 1)) {
+      this.chain = [];
+      throw new CustomError('Incorrect index value')
+    };
+    
+    this.chain.splice(position - 1, 1);
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    this.chain = this.chain.reverse();
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    let res = this.chain.map(val => `( ${val} )`).join('~~');
+    this.chain = [];
+    return res;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
+
+
+
 };
 
 module.exports = chainMaker;
