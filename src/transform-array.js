1b26c6d16b137e8d898852f176d6042c98946487
f19m <leomelik@gmail.com>
2020-10-04 17:54:53 +0500

    fix: debug deled

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,36 +13,30 @@ module.exports = function transform( arr ) {
 
   for(let i = 0; i < arr.length; i++){
     let item = arr[i];
-    log(`item[${i}]=${item}`);
-
+    
     switch (item) {
       case '--discard-next':
-        log('--discard-next');
         if (arr.hasOwnProperty(i + 1)){
           idx = i + 1;
         }  
         break;
       case '--discard-prev':
-        log('--discard-prev');
         if (arr.hasOwnProperty(i - 1) && (i - 1) != idx){
           idx = i - 1;
           resArr.splice(resArr.length-1,1);
         }  
         break;
       case '--double-next':
-        log('--double-next');
         if (arr.hasOwnProperty(i + 1) && (i+1)!= idx ){
           resArr.push(arr[i + 1])
         }
         break;
       case '--double-prev':
-        log('--double-prev');
         if (arr.hasOwnProperty(i - 1) && (i - 1) != idx ){
           resArr.push(arr[i - 1])
         }
         break;
       default:
-        log('--default');
         if (i != idx){
           resArr.push(item)
         }
