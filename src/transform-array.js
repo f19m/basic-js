b98b40675fba0ad8fc0c5ca5e684dab89c3cdbf4
f19m <leomelik@gmail.com>
2020-10-04 17:35:43 +0500

    fix: try to pass last test

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,67 +1,118 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform( arr ) {
- // console.log(arr);
+
 
   if (!(Array.isArray(arr))){
     throw "Input value is not array";
   }
   
-
+  let tmpArr = arr;
   let resArr = [];
   let prevItem=null;
-  let action=null;
+  let idx=0;
 
-  for(let i = 0; i < arr.length; i++){
+  const debugOn = 0;
+
+  const  log=function(abc){
+    if (debugOn){
+      console.log(abc)
+    }
+  }
+ 
+ 
+  //console.log(arr);
+
+  // for(let i = 0; i < arr.length; i++){
   
-      let item = arr[i];
+  //     let item = arr[i];
 
      
-      if (item == '--discard-next' || item == '--double-next'  )
-      {
-        action = item;
-      } else{
-          // item == '--discard-prev' item == '--double-prev'
+  //     if (item == '--discard-next' || item == '--double-next'  )
+  //     {
+  //       action = item;
+  //     } else{
+  //         // item == '--discard-prev' item == '--double-prev'
 
           
 
-          if (action=='--discard-next'){
-            prevItem = null;
-            action = null;
-            continue;
-          }else if(action=='--double-next'){
-            resArr.push(item);
-            resArr.push(item);
-            prevItem = item;
-            action = null;
-            continue;
-          }
+  //         if (action=='--discard-next'){
+  //           prevItem = null;
+  //           action = null;
+  //           continue;
+  //         }else if(action=='--double-next'){
+  //           resArr.push(item);
+  //           resArr.push(item);
+  //           prevItem = item;
+  //           action = null;
+  //           continue;
+  //         }
 
-          if (item=='--discard-prev'){
-            if (prevItem){
-              //   console.log('true=');
-                 resArr.splice(resArr.length-1,1);
-                 prevItem = item;
-            }
-            action = null;
-            //resArr.push(item);
-           // continue;
-          } else if(item=='--double-prev'){
-            if (prevItem){
-              resArr.push(prevItem);
-            }
-           // resArr.push(item);
-            prevItem = item;
-            action = null;
-            //continue;
-          } else {
-            resArr.push(item);
-            prevItem = item;
-          }
+  //         if (item=='--discard-prev'){
+  //           if (prevItem){
+  //             //   console.log('true=');
+  //                resArr.splice(resArr.length-1,1);
+  //                prevItem = item;
+  //           }
+  //           action = null;
+  //           //resArr.push(item);
+  //          // continue;
+  //         } else if(item=='--double-prev'){
+  //           if (prevItem){
+  //             resArr.push(prevItem);
+  //           }
+  //          // resArr.push(item);
+  //           prevItem = item;
+  //           action = null;
+  //           //continue;
+  //         } else {
+  //           resArr.push(item);
+  //           prevItem = item;
+  //         }
 
           
+  //   }
+
+  // }
+
+
+  for(let i = 0; i < arr.length; i++){
+    let item = arr[i];
+    log(`item[${i}]=${item}`);
+
+    switch (item) {
+      case '--discard-next':
+        log('--discard-next');
+        if (arr.hasOwnProperty(i + 1)){
+          tmpArr[i + 1] = '--exclude';
+        }  
+        break;
+      case '--discard-prev':
+        log('--discard-prev');
+        if (arr.hasOwnProperty(i - 1) && tmpArr[i - 1]!='--exclude'){
+          tmpArr[i - 1] = '--exclude';
+          resArr.splice(resArr.length-1,1);
+        }  
+        break;
+      case '--double-next':
+        log('--double-next');
+        if (arr.hasOwnProperty(i + 1) && tmpArr[i+1]!='--exclude' ){
+          resArr.push(arr[i+1])
+        }
+        break;
+      case '--double-prev':
+        log('--double-prev');
+        if (arr.hasOwnProperty(i - 1) && tmpArr[i - 1]!='--exclude' ){
+          resArr.push(arr[i - 1])
+        }
+        break;
+      default:
+        log('--default');
+        if (tmpArr[i]!='--exclude'){
+          resArr.push(item)
+        }
+        break;
     }
-
   }
 
     
